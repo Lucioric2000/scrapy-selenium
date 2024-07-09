0ed4ffb6ea7f11079b598e99dc3d0191ca05bbcc
Jaehong Park <jaehongp@gmail.com>
2024-07-09 18:02:17 +0900

    ...

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from os import path
 # with open('requirements.txt') as reqh:
 #     install_reqs = reqh.readlines()
 
-with open(path.join(path.dirname(__file__), 'VERSION')) as v:
-    VERSION = v.readline().strip()
+# with open(path.join(path.dirname(__file__), 'VERSION')) as v:
+#     VERSION = v.readline().strip()
 
-with open('README.md') as f:
-    long_description = f.read()
+# with open('README.md') as f:
+#     long_description = f.read()
 
 setup(
     # packages=find_packages(),
